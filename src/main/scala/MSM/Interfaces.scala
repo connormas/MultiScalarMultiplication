8a9e34518fc6bc64a8635348ec74c6f5401aaaf1
Connor Masterson <ccmaster@ucsc.edu>
2022-01-18 20:05:30 -0800

    put bundle interfaces in separate file

---
+++ src/main/scala/MSM/Interfaces.scala
@@ -0,0 +1,19 @@
+package MSM
+
+import chisel3._
+
+/* bundle that represents affine (x,y) coordinate */
+class Point(val w: Int) extends Bundle {
+  val x = SInt(w.W)
+  val y = SInt(w.W)
+  override def cloneType = (new Point(w)).asInstanceOf[this.type]
+}
+
+/* bundle that represents projective coordinates where
+*  x = X/X and y = Y/Z*/
+class PointProjective(val w: Int) extends Bundle {
+  val x = SInt(w.W)
+  val y = SInt(w.W)
+  val z = SInt(w.W)
+  override def cloneType = (new PointProjective(w)).asInstanceOf[this.type]
+}
