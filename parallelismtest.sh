d42e7ca7e9c7ea7f350a1a1bc65e15389771661f
Connor Masterson <ccmaster@ucsc.edu>
2022-02-22 16:02:45 -0800

    revised/simplified data collection script

--- parallelismtest.sh
+++ parallelismtest.sh
@@ -16,13 +16,5 @@ fi
 touch $OUTFILE
 
 echo "Starting tests, results will be in $OUTFILE"
-echo ""
-echo "Now doing..."
 
-
-for p in 1 2 4; do #8 16 32 64 128 256 512; do
-    echo "Number of Point Multipliers: $p"
-    
-    sbt test | grep -A 1 "PMNaive Tests starting" >> $OUTFILE
-
-done
+sbt test | grep -A 1 "TopLevelTest" >> $OUTFILE
