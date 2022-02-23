579cd0a8813dee07b1e0754f35e0083a0cca0619
Connor Masterson <ccmaster@ucsc.edu>
2022-02-22 21:23:01 -0800

    new top level with state machine

--- src/main/scala/MSM/TopLevelMSM.scala
+++ src/main/scala/MSM/TopLevelMSM.scala
@@ -3,6 +3,7 @@ package MSM
 import chisel3._
 import chisel3.util.RegEnable
 import chisel3.util.log2Ceil
+import chisel3.util._
 
 /** top level msm module. This module requests input from memory and uses that
  *  data fro its msm computation. It instantiates Point Mult modules and
@@ -12,7 +13,7 @@ import chisel3.util.log2Ceil
  *  @param a: coefficient from definition of elliptic curve
  *  @param p: prime modulus from definition of elliptic curve
  *  */
-class TopLevelMSM(pw: Int, sw: Int, a: Int, p: Int,
+class TopLevelMSM2(pw: Int, sw: Int, a: Int, p: Int,
                   requestsize: Int, numPMmodules: Int) extends Module {
   val io = IO(new Bundle {
     val complete = Input(Bool())
@@ -32,6 +33,12 @@ class TopLevelMSM(pw: Int, sw: Int, a: Int, p: Int,
   //val pointsyvec = VecInit(yregseq)
   val sregseq = io.scalars map {s => RegEnable(s, 0.S, io.load)}
   //val scalarsvec = VecInit(sregseq)
+  val completebits = Reg(Vec(requestsize, Bool()))
+  when (io.load) {
+    for (i <- 0 until requestsize) {
+      completebits(i) := false.B
+    }
+  }
 
 
   // Seq of PointMult Modules, may need to become a Vec later
@@ -46,8 +53,9 @@ class TopLevelMSM(pw: Int, sw: Int, a: Int, p: Int,
     pm.io.load := RegNext(RegNext(io.load))
   }
 
+
   // Point Addition Reduction Module
-  val par = Module(new PAddReduction(numPMmodules, pw, a, p))
+  val par = Module(new PAddReduction2(numPMmodules, pw, a, p))
   PointMults.zipWithIndex foreach { case (pm, i) =>
     par.io.xs(i) := xregseq(i)
     par.io.ys(i) := yregseq(i)
@@ -56,7 +64,8 @@ class TopLevelMSM(pw: Int, sw: Int, a: Int, p: Int,
   // count up number of complete mults
   val MultsComplete = RegInit(0.U(log2Ceil(numPMmodules).W))
   PointMults.zipWithIndex foreach { case (pm, i) =>
-    when (pm.io.valid) {
+    when ((pm.io.valid)  && !completebits(i)) {
+      completebits(i) := true.B
       MultsComplete := MultsComplete + 1.U
       xregseq(i) := pm.io.outx // capture outputs
       yregseq(i) := pm.io.outy
@@ -69,11 +78,19 @@ class TopLevelMSM(pw: Int, sw: Int, a: Int, p: Int,
   val prev = Reg(UInt(log2Ceil(numPMmodules).W))
   curr := MultsComplete
   prev := curr
-  par.io.load := RegNext(curr === 0.U && prev === numPMmodules.U - 1.U)
+  //par.io.load := RegNext(curr === 0.U && prev === numPMmodules.U - 1.U)
+  val readyforreduction = completebits reduce {_ && _}
+  when (RegNext(readyforreduction)) {
+    for (i <- 0 until requestsize) {
+      completebits(i) := false.B
+    }
+  }
+  par.io.load := RegNext(readyforreduction)
+
 
   // latch reduction results
-  val xoutput = RegEnable(par.io.outx, 0.S, par.io.valid)
-  val youtput = RegEnable(par.io.outy, 0.S, par.io.valid)
+  val xreduc = RegEnable(par.io.outx, 0.S, par.io.valid)
+  val yreduc = RegEnable(par.io.outy, 0.S, par.io.valid)
 
   // reset sum logic
   val resetsum = Reg(Bool())
@@ -81,29 +98,167 @@ class TopLevelMSM(pw: Int, sw: Int, a: Int, p: Int,
   val resetonthiscycle = RegInit(false.B)
   resetonthiscycle := resetonthiscycle || resetsum
 
-  /*val pa = Module(new PointAddition(pw))
+  // latch final sum results
+  val xoutput = Reg(SInt(pw.W))
+  val youtput = Reg(SInt(pw.W))
+  xoutput := 0.S
+  youtput := 0.S
+
+  val pa = Module(new PointAddition(pw))
   pa.io.a := a.S
   pa.io.p := p.S
   pa.io.p1x := xoutput
-  pa.io.p1x := youtput
-  pa.io.p2x := par.io.outx
-  pa.io.p2y := par.io.outy
-  pa.io.load := RegNext(par.io.valid)*/
+  pa.io.p1y := youtput
+  pa.io.p2x := xreduc
+  pa.io.p2y := yreduc
+  pa.io.load := RegNext(RegNext(par.io.valid))
 
+  when (pa.io.valid) {
+    xoutput := pa.io.outx
+    youtput := pa.io.outy
+  }
+
+  // reset when next msm computation begins
+  when (resetonthiscycle && io.load && !io.complete) {
+    resetonthiscycle := false.B // issue when next one is single load
+    resetsum := false.B
+    xoutput := 0.S
+    youtput := 0.S
+  }
 
   io.outx := xoutput
   io.outy := youtput
 
-  io.valid := RegNext(par.io.valid)
+  //io.valid := RegNext(par.io.valid)
+  io.valid := RegNext(pa.io.valid) //&& resetonthiscycle
 
-  // debugging
-  /*printf(p"MultsComplete=${MultsComplete}  ")
-  PointMults foreach (pm => printf(p"| ${pm.io.valid} (${pm.io.outx}${pm.io.outy}) |"))
-  printf("\n")
-  xregseq zip yregseq foreach { case (x, y) => printf(p"(${x},${y}) ")}
-  printf("\n")
-  printf(p"par.load=${par.io.load} -> ")
-  par.io.xs.zip(par.io.ys) foreach { case (x, y) => printf(p"(${x},${y}) ")}
-  printf(p" -> (${par.io.outx},${par.io.outy})\n")*/
-  //printf(p"TLMSM -> load=${io.load} multscomplet=${MultsComplete}\n")
+  when (readyforreduction) {
+    printf("\nmultscomplete: ")
+    xregseq zip yregseq foreach { case(x, y) => printf(p"(${x}${y}) ")}
+    printf(p"\n")
+  }
+
+  printf(p"${completebits} load=${io.load} valid${io.valid}\n")
+
+}
+
+
+
+/** second attempt this time with a state machine
+ * */
+
+object TopLevelMSM {
+  val idle :: multiplying :: reduction :: Nil = Enum(3)
+}
+
+
+class TopLevelMSM(pw: Int, sw: Int, a: Int, p: Int,
+                  requestsize: Int, numPMmodules: Int) extends Module {
+  val io = IO(new Bundle {
+    val complete = Input(Bool())
+    val load = Input(Bool())
+    val pointsx = Input(Vec(requestsize, SInt(pw.W)))
+    val pointsy = Input(Vec(requestsize, SInt(pw.W)))
+    val scalars = Input(Vec(requestsize, SInt(sw.W)))
+    val valid = Output(Bool())
+    val outx = Output(SInt(pw.W))
+    val outy = Output(SInt(pw.W))
+  })
+
+  // import states
+  import TopLevelMSM._
+  val state = RegInit(idle)
+
+
+  // make Seq of Regs, holding Points and Scalars (will need Vecs when decoupling)
+  val xregseq = io.pointsx map { x => RegEnable(x, 0.S, io.load) }
+  val yregseq = io.pointsy map { y => RegEnable(y, 0.S, io.load) }
+  val sregseq = io.scalars map { s => RegEnable(s, 0.S, io.load) }
+  val completebits = Reg(Vec(requestsize, Bool()))
+  when(io.load) {
+    for (i <- 0 until requestsize) {
+      completebits(i) := false.B
+    }
+  }
+
+
+  // Seq of PointMult Modules, may need to become a Vec later
+  // connect all point mult modules
+  // set completebit and capture output when a mult is complete
+  val PointMults = Seq.fill(requestsize)(Module(new PMNaive(pw, sw)))
+  PointMults.zipWithIndex foreach { case (pm, i) =>
+    pm.io.a := a.S
+    pm.io.p := p.S
+    pm.io.px := xregseq(i)
+    pm.io.py := yregseq(i)
+    pm.io.s := sregseq(i)
+    pm.io.load := RegNext(RegNext(io.load))
+    when ((pm.io.valid)  && !completebits(i)) {
+      completebits(i) := true.B
+      xregseq(i) := pm.io.outx // capture outputs
+      yregseq(i) := pm.io.outy
+      printf(p"MULTIPLIER ${i} finished, (${pm.io.outx},${pm.io.outy})\n")
+    }
+  }
+
+
+  // Point Addition Reduction Module and check completebits
+  val par = Module(new PAddReduction2(numPMmodules, pw, a, p))
+  PointMults.zipWithIndex foreach { case (pm, i) =>
+    par.io.xs(i) := xregseq(i)
+    par.io.ys(i) := yregseq(i)
+  }
+  val readyforreduction = completebits reduce {_ && _}
+  when (RegNext(readyforreduction)) {
+    for (i <- 0 until requestsize) {
+      completebits(i) := false.B
+    }
+  }
+  par.io.load := RegNext(!readyforreduction && RegNext(readyforreduction))
+
+  // val outputregs
+  val outregx = RegInit(0.S(pw.W))
+  val outregy = RegInit(0.S(pw.W))
+  io.outx := outregx
+  io.outy := outregy
+  io.valid := false.B
+
+  // main switch statement
+  switch (state) {
+    is (idle) {
+      io.valid := false.B
+      when (io.load) {
+        printf("moving to working\n")
+        state := multiplying
+      }
+    }
+    is (multiplying) {
+      when (readyforreduction) {
+        state := reduction
+      }
+    }
+    is (reduction) {
+      outregx := par.io.outx
+      outregy := par.io.outy
+      when (RegNext(par.io.valid)) {
+        state := idle
+        io.valid := true.B
+      }
+    }
+  }
+
+
+  when (RegNext(RegNext(io.load))) {
+    printf("IO.LOAD: ")
+    xregseq zip yregseq zip sregseq foreach { case((x,y), s) => printf(p"(${x},${y})${s} ")}
+    printf("\n")
+  }
+
+  when (readyforreduction) {
+    printf("READYFORREDUCTION: ")
+    xregseq zip yregseq foreach { case(x,y) => printf(p"(${x},${y}) ")}
+    printf("\n")
+  }
+
+  printf(p"${completebits} load=${io.load} valid${io.valid} paddreduc.load(${par.io.load}) result(${par.io.outx},${par.io.outy})\n")
 }
